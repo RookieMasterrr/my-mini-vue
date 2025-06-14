1ffe2267c515ee045dd24d9dea43beb829538a1c
Mobiuss <jf1033224672@gamil.com>
2025-06-14 11:10:32 +0800

    happy path

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,42 @@
+export function createComponentInstance(vnode) {
+    const instance = {
+        vnode,
+        type: vnode.type
+    }
+    return instance
+}
+
+export function setupComponent(instance) {
+    // TODO
+    // initProps
+    // initSlots
+    setupStatefulComponent(instance)
+}
+
+function setupStatefulComponent(instance) {
+    const Component = instance.type
+
+    const { setup } = Component
+
+    if(setup) {
+        const setupResult = setup()
+        handleSetupResult(instance, setupResult)
+    }
+
+}
+function handleSetupResult(instance, setupResult) {
+    // function / Object
+    // TODO function
+    if (typeof setupResult === 'object') {
+        instance.setupState = setupResult
+    }
+
+    finishComponentSetup(instance)
+}
+
+function finishComponentSetup(instance) {
+    const Component = instance.type
+    if(Component.render) {
+        instance.render = Component.render
+    }
+}
---
+++ src/runtime-core/renderer.ts
@@ -0,0 +1,58 @@
+import { isObject } from "../shared";
+import { createComponentInstance, setupComponent } from "./component";
+
+export function render(vnode, container) {
+    patch(vnode, container);
+}
+
+function patch(vnode, container) {
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container);
+    }else if(isObject(vnode.type)) {
+        processComponent(vnode, container);
+    }
+}
+
+function processComponent(vnode, container) {
+    mountComponent(vnode, container);
+}
+
+function processElement(vnode, container) {
+    mountElement(vnode, container);
+}
+
+function mountComponent(vnode, container) {
+    const instance = createComponentInstance(vnode);
+    setupComponent(instance);
+    setupRenderEffect(instance, container)
+}
+
+function mountElement(vnode, container) {
+    const { type,children,props } = vnode
+
+    const el = document.createElement(type)
+
+    if(Array.isArray(children)) {
+        mountChildren(vnode, el)
+    }else if(typeof children === 'string'){
+        el.textContent = children
+    }
+
+    for (const key in props) {
+        const value = props[key]
+        el.setAttribute(key, value)
+    }
+
+    container.append(el)
+}
+
+function mountChildren(vnode, container) {
+    vnode.children.forEach(child => {
+        patch(child, container)
+    });
+}
+
+function setupRenderEffect(instance, container) {
+    const subTree = instance.render()
+    patch(subTree, container)
+}

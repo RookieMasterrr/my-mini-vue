21d546f13c1405907de415a316a0a6cd68eae2a2
Mobiuss <jf1033224672@gamil.com>
2025-06-19 11:11:05 +0800

    feat: TextToArray

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -47,7 +47,7 @@ export function createRender(options) {
     }
     
     function processFragment(n1, n2: any, container: any, parentComponent) {
-        mountChildren(n2, container, parentComponent)
+        mountChildren(n2.children, container, parentComponent)
     }
     
     function processComponent(n1, n2, container, parentComponent) {
@@ -58,33 +58,40 @@ export function createRender(options) {
         if(!n1) {
             mountElement(n2, container, parentComponent);
         }else {
-            patchElement(n1, n2, container)
+            patchElement(n1, n2, container, parentComponent)
         }
     }
     
-    function patchElement(n1, n2, container) {
+    function patchElement(n1, n2, container, parentComponent) {
         // console.log(n1);
         // console.log(n2);
         const oldProps = n1.props || {}
         const newProps = n2.props || {}
 
         const el = n2.el = n1.el
-        patchChildren(n1, n2, el)
+        patchChildren(n1, n2, el, parentComponent)
         patchProps(el, oldProps, newProps)
     }
 
-    function patchChildren(n1, n2, container) {
+    function patchChildren(n1, n2, container, parentComponent) {
         const prevShapeFlag = n1.shapeFlag
-        const shapeFlag = n2.shapeFlag
+        const nextShapeFlag = n2.shapeFlag
         const c1 = n1.children
         const c2 = n2.children
-        if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+        if (nextShapeFlag & ShapeFlags.TEXT_CHILDREN) {
             if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
                 unmountChildren(n1.children)
                 hostSetElementText(container, c2)
             }else if(prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
                 if(c1 === c2) return;
                 hostSetElementText(container, c2)
+            }
+        }else if(nextShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+            if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+                hostSetElementText(container, '')
+                mountChildren(c2, container, parentComponent)
+            }else if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+                
             }
         }
     }
@@ -127,7 +134,7 @@ export function createRender(options) {
         const el = vnode.el = hostCreateElement(type)
     
         if(shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-            mountChildren(vnode, el, parentComponent)
+            mountChildren(vnode.children, el, parentComponent)
         }else if(shapeFlag & ShapeFlags.TEXT_CHILDREN){
             el.textContent = children
         }
@@ -140,8 +147,8 @@ export function createRender(options) {
         hostInsert(el, container)
     }
     
-    function mountChildren(vnode, container, parentComponent) {
-        vnode.children.forEach(child => {
+    function mountChildren(children, container, parentComponent) {
+        children.forEach(child => {
             patch(null, child, container, parentComponent)
         });
     }
